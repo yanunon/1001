04a742928d79a8445b12034bca1dc84023a28e4b
Yang Junyong <yanunon@gmail.com>
2013-04-05 16:01:12 +0800

    add mixed test for go

--- go/sorting/sort_test.go
+++ go/sorting/sort_test.go
@@ -11,25 +11,40 @@ func init() {
 	rand.Seed(time.Now().Unix())
 }
 
-func generateRandomInts(max, length int) *sort.IntSlice {
+func generateRandomInts(max, length int) []int {
 	data := make([]int, length)
 	for i := 0; i < length; i++ {
 		data[i] = rand.Int() % max
 	}
-	is := sort.IntSlice(data)
-	return &is
+	return data
 }
 
 func TestQuickSort(t *testing.T) {
-	data := generateRandomInts(100, 99)
+	data := generateRandomInts(1000, 100)
 	t.Logf("%+v\n", data)
-	QuickSort(data)
+	QuickSort(sort.IntSlice(data))
 	t.Logf("%+v\n", data)
 }
 
 func TestBubbleSort(t *testing.T) {
-	data := generateRandomInts(100, 99)
+	data := generateRandomInts(1000, 100)
 	t.Logf("%+v\n", data)
-	BubbleSort(data)
+	BubbleSort(sort.IntSlice(data))
 	t.Logf("%+v\n", data)
+}
+
+func TestMixed(t *testing.T) {
+	dataQ := generateRandomInts(1000, 100)
+	dataB := make([]int, 100)
+	for i := 0; i < 100; i++ {
+		dataB[i] = dataQ[i]
+	}
+	QuickSort(sort.IntSlice(dataQ))
+	BubbleSort(sort.IntSlice(dataB))
+	for i := 0; i < 100; i++ {
+		if dataB[i] != dataQ[i] {
+			t.Fatalf("dataB[i]:%d != dataQ[i]:%d\n", dataB[i], dataQ[i])
+		}
+	}
+
 }
